e431d989b0fd9165c12594446324ef7945952655
Sakshamkhadka7 <sakshamcode12@gmail.com>
2026-07-12 23:31:50 +0545

    learn collection : tuples,list and set

---
+++ collection.py
@@ -0,0 +1,19 @@
+# countries=["jaban","brazil","portugal"]
+
+# countries.append("india")
+# countries.pop()
+# countries.insert(0,"china")
+# countries.remove("jaban")
+# countries[1]="norway"
+
+# print(len(countries))
+# print(countries)
+
+countries={"japan","usa","nepal"}
+print(countries)
+
+
+# countries_tuples=("argentina","switzerland","france","france")
+
+# print(countries_tuples.count("france"))
+# print(countries_tuples.index("france"))
